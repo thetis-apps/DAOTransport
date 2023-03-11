320e2a3d272037a2fd44e590182ec13f1bc9789f
Lars Møberg-Poulsen <lmp@thetis-ims.com>
2023-03-11 10:54:51 +0000

    2.1.3: Remove dependency on aws-sdk

--- .aws-sam/build/BookingHandler/src/handlers/dao-transport.js
+++ .aws-sam/build/BookingHandler/src/handlers/dao-transport.js
@@ -16,9 +16,6 @@
 
 const axios = require('axios');
 
-var AWS = require('aws-sdk');
-AWS.config.update({region:'eu-west-1'});
-
 /**
  * Send a response to CloudFormation regarding progress in creating resource.
  */
